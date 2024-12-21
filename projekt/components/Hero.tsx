1e082d6f2871ce0ce3c3444d49a8266b8bbf5143
Lam Nguyen <125300659+NotAisu@users.noreply.github.com>
2024-12-21 16:08:33 +0100

    Textgeneratingeffect + text

--- projekt/components/Hero.tsx
+++ projekt/components/Hero.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Spotlight } from './ui/Spotlight'
+import { TextGenerateEffect } from './ui/text-generate-effect'
 
 const Hero = () => {
   return (
@@ -14,10 +15,19 @@ const Hero = () => {
            
                 <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black-100 bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"/>
             </div>
-        <div className="flex justify-center relative my-20 z-10">
-            <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex-col items-center justify-center">
-                <h2 className="uppercase text-center tracking-widest text-xs text-blue-100 max-w-80">Dynamic Web with Next.js</h2>
+            <div className="flex justify-center relative my-20 z-10">
+            <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center">
+                <h2 className="uppercase  tracking-widest text-xs text-center text-blue-100 max-w-80">Dynamic Web Magic with Next.js
+                </h2>
               
+              <TextGenerateEffect 
+                className="text-center text-[40px] md:text-5xl lg:text-6xl"
+                words="Transforming Concepts into Seamless Experiences"
+              />
+
+              <p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl">
+                Hi, I&apos;m Lâm, a Developer based in Germany.
+              </p>
             </div>
         </div>
     </div>
