13b7b2da729a725cf08ed5316df4b19db26dcdd1
Lam Nguyen <125300659+NotAisu@users.noreply.github.com>
2024-12-21 14:42:30 +0100

    Install framermotion + component folder with hero.tsx

--- projekt/app/page.tsx
+++ projekt/app/page.tsx
@@ -1,10 +1,11 @@
+import Hero from "@/components/Hero";
 
 
 export default function Home() {
   return (
     <main className="relative bg-black-100 text-white flex justify-center items-center flex-col overflow-hidden mx-auto sm:px10 px-5">
-    <div>
-      <h1>Welcome to my portfolio</h1>
+    <div className="max-w-7xl w-full">
+      <Hero />
     </div>
     </main>
   );
