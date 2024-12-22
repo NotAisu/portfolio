54165f1768f4bf4b1b2c0fef7ff503e316f77165
Lam Nguyen <125300659+NotAisu@users.noreply.github.com>
2024-12-22 11:39:29 +0100

    navbar + Grid from Bentogrid

---
+++ projekt/data/index.ts
@@ -0,0 +1,7 @@
+export const gridItems = [
+    {title: "title1", description: "description1", id: 1,},
+    {title: "title1", description: "description1", id: 2,},
+    {title: "title1", description: "description1", id: 3,},
+    {title: "title1", description: "description1", id: 4,},
+
+];
