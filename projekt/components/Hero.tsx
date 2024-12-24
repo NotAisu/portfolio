85268abda0b75dd6d9a7cfa4806e552d15eabd32
Lam Nguyen <125300659+NotAisu@users.noreply.github.com>
2024-12-24 11:57:59 +0100

    heropage texte überarbeitet

--- projekt/components/Hero.tsx
+++ projekt/components/Hero.tsx
@@ -19,22 +19,22 @@ const Hero = () => {
             </div>
             <div className="flex justify-center relative my-20 z-10">
             <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center">
-                <h2 className="uppercase  tracking-widest text-xs text-center text-blue-100 max-w-80">Dynamic Web Magic with Next.js
+                <h2 className="uppercase  tracking-widest text-xs text-center text-blue-100 max-w-80">Crafting Digital Solutions
                 </h2>
               
               <TextGenerateEffect 
                 className="text-center text-[40px] md:text-5xl lg:text-6xl"
-                words="Transforming Concepts into Seamless User Experiences"
+                words="Bringing Ideas to Life with Code and Creativity"
               />
 
               <p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl">
-                Hi, I&apos;m Lâm, a Developer based in Germany.
+                Hi, I&apos;m Lâm, a College Student and Developer based in Germany.
               </p>
 
                 <a href="#about">
 
               <MagicButton 
-                title="Show my work"
+                title="Explore My Work"
                 icon={<FaLocationArrow />}
                 position="right"
               />
